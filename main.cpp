a7f80d3b246472761b102935d071263cbf8f488a
Lenovo <ollayorbek2005@gmail.com>
2023-11-08 16:21:29 +0500

    Solved problem2

--- main.cpp
+++ main.cpp
@@ -9,13 +9,16 @@ using namespace std;
 
 int main() {
     float water;
-
+    float i,j,k;
 
     cout << "Problem 1\nEnter the amount of consumed water\n";
     cin>>water;
-    cout<<problemSolution1(water);
-    cout << "Problem 2\n";
-    //problemSolution2();
+    cout<<problemSolution1(water)<<endl;
+    cout << "Problem 2\nEnter three numbers\n";
+    cin>>i;
+    cin>>j;
+    cin>>k;
+    cout<<"The maximum number is "<<problemSolution2(i,j,k)<<endl;
     cout << "Problem 3\n";
     //problemSolution3();
     cout << "Problem 4\n";
--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,11 @@
 float problemSolution2(float i, float j, float k) {
     float result;
-    // write your code here;
-
+    if(i>=j&&i>=k){
+        result = i;
+    } else if(j>=k){
+        result = j;
+    } else{
+        result = k;
+    }
     return result;
 }
